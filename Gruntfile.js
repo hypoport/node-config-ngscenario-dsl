c64a45bc4d1ab4b04d653c98849accad54600e2e
Andreas Marek <andimarek@fastmail.fm>
2013-07-27 20:19:38 +0200

    added grunt file: default task executes all tests

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  var initConfig = {
+
+    jasmine_node: {
+      projectRoot: "./",
+      requirejs: false,
+      forceExit: true,
+      jUnit: true,
+      coffee: false
+    },
+    karma: {
+      e2e: {
+        configFile: 'int-tests/karma.conf.js'
+      }
+    }
+  };
+
+  grunt.initConfig(initConfig);
+
+  grunt.registerTask('start-int-test-webserver', 'Start the web server for int-tests.', function () {
+    require('./int-tests/server.js');
+  });
+
+  grunt.registerTask('int-test', [
+    'start-int-test-webserver',
+    'karma:e2e'
+  ]);
+
+  grunt.registerTask('test',['jasmine_node','int-test'])
+  grunt.registerTask('default', ['test']);
+
+};
